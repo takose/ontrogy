d5b071ca6235c6088e1dd38846772130a8e08e05
tako <rina.m.takase@gmail.com>
2017-07-28 23:51:22 +0900

    rails g model ingredient category:string name:string:unique && bin/rails db:migrate

---
+++ db/migrate/20170728144824_create_ingredients.rb
@@ -0,0 +1,10 @@
+class CreateIngredients < ActiveRecord::Migration[5.1]
+  def change
+    create_table :ingredients do |t|
+      t.string :category
+      t.string :name, unique: true
+
+      t.timestamps
+    end
+  end
+end
